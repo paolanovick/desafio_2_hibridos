85a032d378e7f43627103b20e596afe70f16b129
Paola Novick <paola.novick@davinci.edu.ar>
2024-08-26 18:03:54 -0300

    Modificar primo.js para usar CommonJS y agregar index.mjs y primo.mjs

---
+++ Ejercicio 1/primo.mjs
@@ -0,0 +1,18 @@
+// primo.js
+
+/**
+ * Verifica si un número es primo.
+ * @param {number} num - El número a verificar.
+ * @returns {boolean} - Retorna true si el número es primo, false en caso contrario.
+ */
+function esPrimo(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+    for (let i = 5; i * i <= num; i += 6) {
+        if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+    return true;
+}
+
+module.exports = esPrimo; // Exporta usando CommonJS
